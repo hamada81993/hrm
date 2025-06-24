54e60deb2fa825c0ab7e968ee2dbfe1d481f91ac
ahmed haroun <43412584+hamada81993@users.noreply.github.com>
2025-06-25 00:13:29 +0300

    Update Attendance.jsx

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -14,7 +14,7 @@ const Attendance = () => {
   })
   const [editingId, setEditingId] = useState(null)
 
-  const API_BASE_URL = 'http://spl-pro.com/hr/api'
+  const API_BASE_URL = 'https://spl-pro.com/hr/api'
 
   useEffect(() => {
     fetchAttendances()
