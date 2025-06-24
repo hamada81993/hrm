a7a647c6ee805e8b63e002ceb59fef1c534a4383
ahmed haroun <43412584+hamada81993@users.noreply.github.com>
2025-06-25 00:11:37 +0300

    Update OtherPayments.jsx

--- src/pages/OtherPayments.jsx
+++ src/pages/OtherPayments.jsx
@@ -16,7 +16,7 @@ const OtherPayments = () => {
   })
   const [editingId, setEditingId] = useState(null)
 
-  const API_BASE_URL = 'http://spl-pro.com/hr/api'
+  const API_BASE_URL = 'https://spl-pro.com/hr/api'
 
   const paymentTypes = [
     'تكلفة الإقامة',
